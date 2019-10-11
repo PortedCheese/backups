6667c7f740edf50ec7a0de5bf1ae385b5958f0a3
Vladimir <vladimirpeshkov5dev@yandex.ru>
2019-10-11 16:13:17 +0300

    change helper function

--- src/helpers.php
+++ src/helpers.php
@@ -9,6 +9,7 @@ if (! function_exists('backup_path')) {
      */
     function backup_path($path = '')
     {
-        return app('path.storage').($path ? DIRECTORY_SEPARATOR."app/backups/$path" : "app/backups");
+        $folder = DIRECTORY_SEPARATOR."app".DIRECTORY_SEPARATOR."backups";
+        return app('path.storage').($path ? $folder.DIRECTORY_SEPARATOR.$path : $folder);
     }
 }
